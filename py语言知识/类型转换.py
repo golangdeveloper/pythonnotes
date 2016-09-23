8e8ea939105d2f5b74a54ef22ce3a4a3908289f6
timeloveboy <734991033@qq.com>
2016-09-23 12:06:46 +0800

    ~

---
+++ py语言知识/类型转换.py
@@ -0,0 +1,28 @@
+
+int(x [,base ])         # 将x转 换为一个整数  （其中base表示基数）
+
+long(x [,base ])        将 x转换为一个长整数
+
+float(x )               将 x转换到一个浮点数
+
+complex(real [,imag ])  创建一个复数
+
+str(x )                 将 对象 x 转换为字符串
+
+repr(x )                将对象 x 转换为表达式字符串
+
+eval(str )              用来计算在字符串中的有效Python表达式,并返回一个对象
+
+tuple(s )               将 序列 s 转换为一个元组
+
+list(s )                将序列 s 转换为一个列 表
+
+chr(x )                 将一个整数转换为一个字符
+
+unichr(x )              将 一个整数转换为Unicode字符
+
+ord(x )                 将一个字符转换为 它的整数值
+
+hex(x )                 将一个整数转换为一个十六进制字符串
+
+oct(x )                 将 一个整数转换为一个八进制字符串
